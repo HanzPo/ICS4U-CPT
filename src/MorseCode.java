fed3b56bc9dd782155f733dd811faa654d26dd60
Hanz Po <hanznathanpo@gmail.com>
2023-06-08 17:05:23 -0400

    Add UI

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -6,17 +6,17 @@
 
 // Defines class. Used statically in EnglishToMorse and MorseToEnglish.
 public class MorseCode {
-	
+
 	// Array to store morse code letters. SHOULD NOT BE CHANGED!!
 	private static final String[] morseLetters = new String[]{ "* _", "_ * * *", "_ * _ *", "_ * *", "*", "* * _ *", "_ _ *", "* * * *", "* *", "* _ _ _", "_ * _", "* _ * *", "_ _", "_ *", "_ _ _", "* _ _ *", "_ _ * _", "* _ *", "* * *", "_", "* * _", "* * * _", "* _ _", "_ * * _", "_ * _ _", "_ _ * *"};
 
 	// Method to encrypt morse code, to be called from other classes. Trims input string.
-	public static String MorseEncrypt(String str) {
-		return MorseEncryptRecursive(str.trim());
+	public static String morseEncrypt(String str) {
+		return morseEncryptRecursive(str.trim());
 	}
-	
+
 	// Method for actual encryption using recursion. Input trimming was separated in order to prevent weird things from happening with spaces.
-	private static String MorseEncryptRecursive(String englishText) {
+	private static String morseEncryptRecursive(String englishText) {
 		
 		// Set text to uppercase
 		englishText = englishText.toUpperCase();
@@ -28,7 +28,7 @@ public class MorseCode {
 		
 		// If input is a space, return eight spaces, because eight spaces in morse code represents one space in plain text. Pass the rest of the string in recursively
 		if (englishText.charAt(0) == ' ') {
-			return "        " + MorseEncryptRecursive(englishText.substring(1));
+			return "        " + morseEncryptRecursive(englishText.substring(1));
 		}
 		
 		// Get index of the morse code letter to use, based on the current character minus 65 (which is the ASCII code for "A")
@@ -36,64 +36,80 @@ public class MorseCode {
 		
 		// Checks to see if current character is in the alphabet. If true
 		if (index >= 0 && index <= 25) {
-			return morseLetters[index] + "    " + MorseEncryptRecursive(englishText.substring(1));
+			return morseLetters[index] + "    " + morseEncryptRecursive(englishText.substring(1));
 		}
 		
 		// If current character is not alphabetical, return the same character
-		return englishText.charAt(0) + "    " +  MorseEncryptRecursive(englishText.substring(1));
+		return englishText.charAt(0) + "    " +  morseEncryptRecursive(englishText.substring(1));
 	}
 	
 	// Method to decrypt morse code, to be called from other classes. Trims input string.
-	public static String MorseDecrypt(String str) {
-		return MorseDecryptRecursive(str.trim());
+	public static String morseDecrypt(String str) {
+		return morseDecryptRecursive(str.trim());
 	}
 	
-	private static String MorseDecryptRecursive(String morseText) {
+	// Method for actual decryption using recursion. Input trimming was separated in order to prevent weird things from happening with spaces.
+	private static String morseDecryptRecursive(String morseText) {
 		
+		// Base case, if input string is empty return empty string
 		if (morseText.length() < 1) {
 			return "";
 		}
 		
+		// Looks for the location of the next gap between morse letters (equivalent to 4 spaces)
 		int indexOfNextGap = morseText.indexOf("    ");
+		// Looks for the location of the next morse code space (equivalent to 8 spaces)
 		int indexOfNextSpace = morseText.indexOf("        ");
+		// Used to store the value of the current character being converted, from 0-25, based on its position in the alphabet. If the current character is not alphabetical, such as a punctuation mark, the value is -1.
+		int currentChar;
 		
+		// If a space is at the front of the input string, return a plain english space, then pass the rest of the input into the recursive function
 		if (indexOfNextSpace == 0) {
-			return " " + MorseDecryptRecursive(morseText.substring(8));
+			return " " + morseDecryptRecursive(morseText.substring(8));
 		}
 		
+		// If a gap between letters is at the front of the input string, just pass the rest of the input into the recursive function
 		if (indexOfNextGap == 0) {
-			return "" + MorseDecryptRecursive(morseText.substring(4));
+			return morseDecryptRecursive(morseText.substring(4));
 		}
-		
-		
+
+		// If no more gaps exist, that means there is only one letter left to convert
 		if (indexOfNextGap == -1) {
-			int currentChar = getIndexFromItem(morseText, morseLetters);
+			// Sets the current character to a value from -1 to 25. If the number is from 0-25, that means that the current character exists in the morse code array. If the number is -1, it is not in the alphabet. This includes characters such as punctuation marks.
+			currentChar = getIndexFromItem(morseText, morseLetters);
 			
+			// If the current character is not alphabetical, simply return that character
 			if (currentChar == -1) {
 				return String.valueOf(morseText.charAt(0));
 			}
 			
+			// If it is alphabetical, add 65 to get the ASCII code, convert to a String, then return.
 			return String.valueOf((char) (currentChar + 65));
 		}
 
-		int currentChar = getIndexFromItem(morseText.substring(0, indexOfNextGap), morseLetters);
+		// Sets the current character to a value from -1 to 25. If the number is from 0-25, that means that the current character exists in the morse code array. If the number is -1, it is not in the alphabet. This includes characters such as punctuation marks.
+		currentChar = getIndexFromItem(morseText.substring(0, indexOfNextGap), morseLetters);
 		
+		// If the current character is not alphabetical, simply return that character. Call the method recursively while passing in the rest of the input
 		if (currentChar == -1) {
-			return morseText.charAt(0) + MorseDecryptRecursive(morseText.substring(1));
+			return morseText.charAt(0) + morseDecryptRecursive(morseText.substring(1));
 		}
 		
-		return String.valueOf((char) (currentChar + 65)) + MorseDecryptRecursive(morseText.substring(indexOfNextGap));
-	}
+		// If it is alphabetical, add 65 to get the ASCII code, convert to a String, then return. Call the method recursively while passing in the rest of the input
+		return String.valueOf((char) (currentChar + 65)) + morseDecryptRecursive(morseText.substring(indexOfNextGap));
+	} // End of morseDecryptRecursive
 
 	// Gets index of an item from a given array. If the item cannot be found, return -1
 	private static int getIndexFromItem(String item, String[] array) {
 		
+		// Iterate through given array looking for item. Return i if found.
 		for (int i = 0; i < array.length; i++) {
 			if (item.equals(array[i])) {
 				return i;
 			}
 		}
 		
+		// Return -1 if nothing is found
 		return -1;
-	}
-}
+	} // End of getIndexFromItem
+} // End of MorseCode class
--- src/MorseToEnglish.java
+++ src/MorseToEnglish.java
@@ -1,59 +1,71 @@
 /* Created by: Hanz Nathan Po
  * Date created: May 25, 2023
- * Last updated: June 6, 2023
+ * Last updated: June 8, 2023
  * Description: Program that allows users to specify a file to read Morse Code from, convert to English, then write to the same file.
  */
 
-import java.io.File;
-import java.io.FileWriter;
-import java.util.LinkedList;
-import java.util.Scanner;
+ // Package imports
+ import java.io.File;
+ import java.io.FileWriter;
+ import java.util.Scanner;
+ import javax.swing.*;
+ 
+ // Start of EnglishToMorse class, main class which converts morse code to plain english text
+ public class MorseToEnglish {
+	// Main method, entry point of program. All functionality is contained within here for this program
+	 public static void main(String[] args) {
+ 
+		 // Declare fileName variable. Stores the name of the file that the user would like to convert
+		 String fileName;
+ 
+		 // Assignment of file name value, with default of "src/Morse.txt"
+		 fileName = JOptionPane.showInputDialog(null, "Please enter the path of the file you would like to convert:", "src/Morse.txt");
+ 
+		 // If the user presses cancel, closes the window, or does not enter a file name, exit the program
+		 if (fileName == null) {
+			 System.exit(1);
+		 }
+ 
+		 // Try catch to check if the file exists and can be opened. Otherwise, exit program
+		 try {
+			// Declarations
+			 Scanner input; // Used to read file
+			 StringBuilder builder; // Used to store user input for conversion
+			 String output; // Used for storing converted text
+			 FileWriter writer; // Used to write to file
+			 
+			 input = new Scanner(new File(fileName)); // Creates new Scanner object and tells it to read from file
+			 builder = new StringBuilder(); // Creates new StringBuilder object
 
-import javax.swing.*;
+			 // Keep reading input file until there are no more lines left
+			 while (input.hasNext()) {
+				// Append the current line to the stringbuilder
+				 builder.append(input.nextLine() + "\n");
+			 }
 
+			 // Closes scanner when no longer in use in order to prevent issues
+			 input.close();
+ 
+			 // Displays unconverted text
+			 JOptionPane.showMessageDialog(null, builder, "Showing current contents of " + fileName, JOptionPane.PLAIN_MESSAGE);
+			 
+			 writer = new FileWriter(fileName, false); // Creates new FileWriter object and tells it to write to file, overwriting any previous data
+			 output = MorseCode.morseDecrypt(builder.toString()); // Converts contents of the String Builder and stores in output
 
-public class MorseToEnglish {
-	public static void main(String[] args) {
-		
-		
-		String fileName;
-		LinkedList<String> lines = new LinkedList<>();
-
-		fileName = JOptionPane.showInputDialog(null, "Please enter the path of the file you would like to convert:", "src/English.txt");
-
-		if (fileName == null) {
-			System.exit(1);
-		}
-
-		try {
-			Scanner input = new Scanner(new File(fileName));
-			StringBuilder output = new StringBuilder();
-			String currentLine;
-			
-			while (input.hasNext()) {
-				currentLine = input.nextLine();
-				
-				lines.add(currentLine);
-			}
-			
-			input.close();
-
-
-			
-			FileWriter writer = new FileWriter(fileName, false);
-			
-			for (int i = 0; i < lines.size(); i++) {
-				lines.set(i, MorseCode.MorseDecrypt(lines.get(i)));
-				writer.write(lines.get(i) + "\n");
-			}
-			
-			writer.close();
-		}
-		catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e, e.getClass().toString(), JOptionPane.ERROR_MESSAGE);
-			System.exit(1);
-		}
-		
-		System.exit(0);
-	}
-}
+			 writer.write(output); // Writes output to file
+			 writer.close(); // Closes FileWriter when no longer in use to prevent issues
+			 
+			 // Shows converted text
+			 JOptionPane.showMessageDialog(null, output, "Showing contents of " + fileName + " converted to English", JOptionPane.PLAIN_MESSAGE);
+ 
+		 }
+		 catch (Exception e) {
+			// Show user what the error is, then exit
+			 JOptionPane.showMessageDialog(null, e, e.getClass().toString(), JOptionPane.ERROR_MESSAGE);
+			 System.exit(1);
+		 }
+		 
+		 // Exit program normally
+		 System.exit(0);
+	 } // End of main method
+ } // End of MorseToEnglish class
